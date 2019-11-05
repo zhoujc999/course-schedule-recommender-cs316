36bb4c3b26ecb9a15b8a55522b977884fcda84ba
Jingchao Zhou <zhoujc999@gmail.com>
2019-11-05 14:57:12 -0500

    Add Django Backend.

--- my-app/backend/settings.py
+++ my-app/backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
 
     # 3rd party apps
     'rest_framework',
