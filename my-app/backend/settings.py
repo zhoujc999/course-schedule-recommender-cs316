1d132fc8d245633b85652f348bf06a124625e6d9
Michael Nicholson <man44@duke.edu>
2019-11-06 07:31:13 -0500

    Added potenial database connection, currently commented out

--- my-app/backend/settings.py
+++ my-app/backend/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-import dj_database_url
-import dotenv
+#import dj_database_url
+#import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
