fb2ae0546404f294f48353e5b5feeb310d6372d7
Jingchao Zhou <zhoujc999@gmail.com>
2019-11-16 21:23:51 -0500

    Add firebase-auth backend

--- recommender/views.py
+++ recommender/views.py
@@ -1,6 +1,9 @@
 from django.views.decorators.cache import never_cache
 from django.views.generic.base import TemplateView
 
+
+from django.contrib.auth.models import User
+
 from recommender.models import Program,     \
                                Class,       \
                                Student,     \
@@ -13,26 +16,39 @@ from recommender.serializers import ProgramSerializer,     \
                                     CompletedSerializer
 
 from rest_framework import views, viewsets
+from rest_framework import generics
+from rest_framework import filters
+from rest_framework import status
 from rest_framework import response
+from rest_framework.permissions import IsAuthenticated
 
 index = never_cache(TemplateView.as_view(template_name='index.html'))
 
-class ProgramView(viewsets.ReadOnlyModelViewSet):
+class ProgramView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ProgramSerializer
-    queryset = Program.objects.all()
+    def get_queryset(self):
+        user = self.request.user
+        queryset = WagePosting.objects.filter(uid=user)
+        return queryset
+    #  queryset = Program.objects.all()
 
-class ClassView(viewsets.ReadOnlyModelViewSet):
+class ClassView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ClassSerializer
     queryset = Class.objects.all()
 
-class StudentView(viewsets.ReadOnlyModelViewSet):
+class StudentView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
 
-class SemesterView(viewsets.ReadOnlyModelViewSet):
+class SemesterView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = SemesterSerializer
     queryset = Semester.objects.all()
 
-class CompletedView(viewsets.ReadOnlyModelViewSet):
+class CompletedView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = CompletedSerializer
     queryset = Completed.objects.all()
