4b38416b5ca33ec0afa6d5f68540556c995f3bda
David Kohler <dako.davidkohler@gmail.com>
2019-11-21 23:32:00 -0500

    [add] started adding course schedule

--- src/components/Account.js
+++ src/components/Account.js
@@ -8,15 +8,17 @@ import './stylesheets/Account.css';
 
 const DUMMY_ACCOUNT_INFO = {netid: "joe24", bio: "A pasta expert"};
 const DUMMY_COMPLETED = [{name: "Culinary Arts", type: "B.A."}, {name: "Astrophysics", type: "Ph.D."}];
+// const DUMMY_SEMESTERS = [];
 const DUMMY_SEMESTERS = [
-  { sem_num: "1", courses: [ {code: "ART101", name: "Art101", taken_for: "Culinary Arts B.A"}, {code: "PASTA101", name: "Cooking101", taken_for: "Culinary Arts B.A"} ] },
-  { sem_num: "2", courses: [ {code: "PASTA102", name: "The Art of Spaghetti", taken_for: "Culinary Arts B.A"} ] },
-  { sem_num: "3", courses: [ {code: "PASTA201", name: "Macaroni Art", taken_for: "Culinary Arts B.A"} ] },
-  { sem_num: "4", courses: [ {code: "PASTA305", name: "Linguini Painting", taken_for: "Culinary Arts B.A"}, {code: "PASTA255", name: "Tortellini Sculpting", taken_for: "Culinary Arts B.A"} ] },
-  { sem_num: "5", courses: [ {code: "PASTA420", name: "Writing Orzo", taken_for: "Culinary Arts B.A"}, {code: "PASTA465", name: "Phtographing Capellini", taken_for: "Culinary Arts B.A"} ] },
+  { sem_num: 1, courses: [ {code: "ART101", name: "Art101", taken_for: "Culinary Arts B.A"}, {code: "PASTA101", name: "Cooking101", taken_for: "Culinary Arts B.A"} ] },
+  { sem_num: 2, courses: [ {code: "PASTA102", name: "The Art of Spaghetti", taken_for: "Culinary Arts B.A"} ] },
+  { sem_num: 3, courses: [ {code: "PASTA201", name: "Macaroni Art", taken_for: "Culinary Arts B.A"} ] },
+  { sem_num: 4, courses: [ {code: "PASTA305", name: "Linguini Painting", taken_for: "Culinary Arts B.A"}, {code: "PASTA255", name: "Tortellini Sculpting", taken_for: "Culinary Arts B.A"} ] },
+  { sem_num: 5, courses: [ {code: "PASTA420", name: "Writing Orzo", taken_for: "Culinary Arts B.A"}, {code: "PASTA465", name: "Phtographing Capellini", taken_for: "Culinary Arts B.A"} ] },
 ];
 const DUMMY_TYPES = [{type: "B.A.", label: "B.A."}, {type: "B.S.", label: "B.S."}, {type: "Concentration", label: "Concentration"}, {type: "Ph.D.", label: "Ph.D."}, {type: "Minor", label: "Minor"}];
 const DUMMY_PROGRAMS = [{name: "Computer Science", label: "Computer Science"}, {name: "Psychology", label: "Psychology"}, {name: "Astrophysics", label: "Astrophysics"}, {name: "Culinary Arts", label: "Culinary Arts"}];
+const DUMMY_COURSES = [{code: "ART101"}, {code: "PASTA102"}, {code: "PASTA201"}, {code: "PASTA101"}, {code: "PASTA305"}, {code: "PASTA255"}, {code: "PASTA420"}, {code: "PASTA465"}, {code: "MATH212"}, {code: "CHEM101"}];
 
 class Account extends Component {
   constructor(props) {
@@ -27,6 +29,7 @@ class Account extends Component {
       semesters: [], //Sems of classes completed -> Semester + Class DB
       bioUpdated: "INITIAL",
       progUpdated: "INITIAL",
+      semsUpdated: "INITIAL",
       newSemesters: [],
     };
     this.handleBioUpdate = this.handleBioUpdate.bind(this);
@@ -34,6 +37,10 @@ class Account extends Component {
     this.handleProgramUpdate = this.handleProgramUpdate.bind(this);
     this.handleProgramChange = this.handleProgramChange.bind(this);
     this.handleTypeChange = this.handleTypeChange.bind(this);
+    this.handleSemesterUpdate = this.handleSemesterUpdate.bind(this);
+    this.handleCourseChange = this.handleCourseChange.bind(this);
+    this.handleAddCourse = this.handleAddCourse.bind(this);
+    this.handleRemoveCourse = this.handleRemoveCourse.bind(this);
   }
 
   handleBioSubmit(e) {
@@ -98,6 +105,39 @@ class Account extends Component {
     } else {
       this.setState({ progUpdated: "FAILED" });
     }
+  }
+
+  handleCourseChange = (i, s_i) => selectedValue => {
+    //Handles change in semester s_i course selection at index i
+    let semesters = [...this.state.semesters];
+    semesters.find(val => val.sem_num === s_i)
+      .courses[i].code = selectedValue[0].code;
+    this.setState({ semesters, semsUpdated: "PENDING" });
+  }
+
+  handleAddCourse = s_i => {
+    //Handles adding a course to semester s_i
+    let semesters = [...this.state.semesters];
+    semesters.find(val => val.sem_num === s_i)
+      .courses.push({ code: ""} );
+    this.setState({ semesters, semsUpdated: "PENDING" });
+  }
+
+  handleRemoveCourse = s_i => {
+    //Handles removing a course to semester s_i
+    let semesters = [...this.state.semesters];
+    semesters.find(val => val.sem_num === s_i).courses =
+      semesters.find(val => val.sem_num === s_i).courses.slice(0, -1);
+    this.setState({ semesters, semsUpdated: "PENDING" });
+  }
+
+  handleSemesterUpdate() {
+    //This function should be "final" confirmation of semester change
+    //TODO: Send updated info to backend
+    //TODO: Get rid of console logs
+    console.log('updating backend');
+    console.log(this.state);
+    this.setState({ semsUpdated: "SUCCESS" });
   }
 
   renderAccountInfo() {
@@ -312,7 +352,7 @@ class Account extends Component {
               Add Program
             </Button>
           </div>
-          <div className="completed_remove_button">
+          <div>
             <Button
               variant="secondary"
               size="sm"
@@ -363,9 +403,31 @@ class Account extends Component {
 
 
   renderEmptySemesters() {
+    const { semesters } = this.state;
     return (
       <div className="semesters_empty">
-        Semesters empty
+        <span className="semester_header">Course Schedule</span>
+        <hr className="hrstyle_main" />
+        <div className="sems_empty_main">
+          <span className="no_sem_text">
+            You have no course schedule! Please add your courses and semesters below
+          </span>
+          <div className="sems_add_button">
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={() =>
+                this.setState({
+                  semesters: [...semesters, {sem_num: 1, courses: []}],
+                  semsUpdated: "PENDING"
+                })
+              }
+            >
+              Add Semester
+            </Button>
+          </div>
+        </div>
+        <hr className="hrstyle_separator" />
       </div>
     );
   }
@@ -390,25 +452,188 @@ class Account extends Component {
     return courses;
   }
 
-  renderSemesters() {
-    const { semesters } = this.state;
-    const sems = [];
-    for (let i = 0; i < semesters.length; i++) {
-      sems.push(
-        <div className="sem_semester" key={i}>
-          <div className="sem_number">
-            Semester #: {semesters[i].sem_num}
-          </div>
-          <div className="sem_classes">
-            {this.bundleCourses(semesters[i].courses)}
-          </div>
-        </div>);
-    }
+  emptyCourses(semester) {
     return (
-      <div className="semesters_filled">
-        {sems}
+      <div className="empty_course">
+        <span className="no_course_text">
+          No courses listed for this semester! Please add your courses below
+        </span>
+        <div className="course_add_button">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => this.handleAddCourse(semester.sem_num)}
+          >
+            Add Course
+          </Button>
+        </div>
       </div>
     );
+  }
+
+  makeCourses(semester) {
+    const coursePlaceholder = "ex. AMI215/MATH212/etc.";
+    const courses = semester.courses.map((course, i) => (
+      <div className="indiv_course" key={i}>
+        <div className="course_top_row">
+          <div className="course_label">Course:</div>
+          <i
+            className="fas fa-question-circle"
+            data-tip data-for="course_name"
+            style={{color: "#17a2b8", padding: "8px 4px 0px 0px"}}
+          />
+          <ReactTooltip
+            id="course_name"
+            place="right"
+            type="info"
+            effect="float"
+          >
+            {"The code associated with your course (ex. AMI215)"}
+            <br/>
+            {'If you do not see your course listed, type it in and add it'}
+          </ReactTooltip>
+          {
+            course.name === "" &&
+            this.renderWarningIcon("Please complete this field")
+          }
+        </div>
+        <div className="course_select">
+          <Select
+            searchable
+            create
+            labelField={"code"} //field of object
+            valueField={"code"} //change field to key in real data
+            searchBy={"code"}
+            placeholder={ coursePlaceholder }
+            addPlaceholder={ course.code === "" && coursePlaceholder }
+            onChange={ this.handleCourseChange(i, semester.sem_num) }
+            values={
+              [course.code !== "" &&
+              DUMMY_COURSES.find(val => val.code === course.code)]
+            }
+            options={ DUMMY_COURSES }
+          />
+        </div>
+      </div>
+    ));
+    return (
+      <div>
+        {courses}
+        <div className="course_add_button">
+          <Button
+            disabled={courses.length >= 8}
+            variant="secondary"
+            size="sm"
+            onClick={() => this.handleAddCourse(semester.sem_num)}
+          >
+            Add Course
+          </Button>
+        </div>
+        <div className="course_remove_button">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => this.handleRemoveCourse(semester.sem_num)}
+          >
+            Remove Course
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  renderSemesters() {
+
+    //Render schedule with at least one semester
+    //TODO: replace DUMMY_SEMESTERS with real data
+    const { semesters, semsUpdated } = this.state;
+    // const s = "ex. Psychology/Statistics/etc.";
+    // const typePlaceholder = "ex. B.S./Minor/etc.";
+    const sems = this.state.semesters.map((sem, i) => (
+        <div className="indiv_semester" key={i}>
+          <div className="semester_label">Semester: {sem.sem_num}</div>
+          <div className="courses">
+            {sem.courses.length > 0
+              ? this.makeCourses(sem)
+              : this.emptyCourses(sem)
+            }
+          </div>
+        </div>
+      )
+    );
+    return (
+      <div>
+        <span className="semester_header">Course Schedule</span>
+        <hr className="hrstyle_main" />
+        {sems}
+        <div className="semester_buttons">
+          <div className="sems_add_button">
+            <Button
+              variant="secondary"
+              size="sm"
+              disabled={this.state.semesters.length >= 10}
+              onClick={() =>
+                this.setState({
+                  semesters: [...semesters, {sem_num: this.state.semesters.length + 1, courses: []}],
+                  semsUpdated: "PENDING"
+                })
+              }
+            >
+              Add Semester
+            </Button>
+          </div>
+          <div>
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={() =>
+                this.setState({
+                  semesters: [...semesters.slice(0,-1)],
+                  semsUpdated: "PENDING"
+                })
+              }
+            >
+              Remove Semester
+            </Button>
+          </div>
+        </div>
+        <Button
+          variant="primary"
+          size="sm"
+          onClick={this.handleSemesterUpdate}
+          disabled={ semsUpdated === "SUCCESS" || semsUpdated === "INITIAL" }
+        >
+          Update
+        </Button>
+        {semsUpdated === "SUCCESS" && (
+          <span className="success_text">Successfully Updated!</span>
+        )}
+        {semsUpdated === "FAILED" && (
+          <span className="failed_text">
+            Something went wrong... Please check error icons and try again
+          </span>
+        )}
+        <hr className="hrstyle_separator" />
+      </div>
+    );
+    // const { semesters } = this.state;
+    // const sems = [];
+    // for (let i = 0; i < semesters.length; i++) {
+    //   sems.push(
+    //     <div className="sem_semester" key={i}>
+    //       <div className="sem_number">
+    //         Semester #: {semesters[i].sem_num}
+    //       </div>
+    //       <div className="sem_classes">
+    //         {this.bundleCourses(semesters[i].courses)}
+    //       </div>
+    //     </div>);
+    // }
+    // return (
+    //   <div className="semesters_filled">
+    //     {sems}
+    //   </div>
+    // );
   }
 
   /*********************************************************************************/
