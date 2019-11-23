f1ec5feb0f38e3586dfe78641a7b1ec3c05bd0f0
Jason Zhou <jz192@duke.edu>
2019-11-23 18:21:30 +0000

    Resolve database user issue.

--- students/models.py
+++ students/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from recommender.models import Student
 
 class User(AbstractUser):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE,
+                                   null=True, blank=True)
     firebase_uid = models.CharField(max_length=64, unique=True, default='')
     class Meta:
         db_table = 'students_user'
