50bf94075adccb5a9dbd8c644df47262817f6799
David Kohler <dako.davidkohler@gmail.com>
2019-12-07 17:42:10 -0500

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -65,7 +65,8 @@ class Home extends Component {
     .then(res => {
       console.log('5')
       console.log(res)
-      return res.json()).then(res => {
+      return res.json();
+    ).then(res => {
       console.log('6')
       console.log(res);
       const programs = JSON.parse(res);
