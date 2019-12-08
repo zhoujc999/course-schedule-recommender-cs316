74227ec09ed53b43e8686e4a0473a7a7f7a487a6
Karen Ou <karenoujia@gmail.com>
2019-12-07 22:01:20 -0500

    change regex

--- src/components/Account.js
+++ src/components/Account.js
@@ -110,8 +110,8 @@ class Account extends Component {
     console.log('updating backend');
     console.log(this.state);
 
-    updateProgramUrl = "https://course-schedule-recommender.herokuapp.com/api/completedbynetid?netid=man44";
-    axios.post()
+    const updateProgramUrl = "https://course-schedule-recommender.herokuapp.com/api/completedbynetid?netid=man44";
+    //axios.post()
 
     const { completed } = this.state;
     let canUpdate = true;
