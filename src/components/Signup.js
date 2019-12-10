58155ed54954d2e1d1145ae95a46482c0f23a904
David Kohler <dako.davidkohler@gmail.com>
2019-12-10 09:10:36 -0500

    test

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,6 +7,8 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Account from './Account';
+
 import './stylesheets/Signup.css';
 
 class Signup extends Component {
