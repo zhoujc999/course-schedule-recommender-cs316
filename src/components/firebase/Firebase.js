e5e7d161be9f9f09ea6575c9ae74430370b8a4ba
Jingchao Zhou <zhoujc999@gmail.com>
2020-01-09 10:57:47 -0500

    Update Firebase.js

--- src/components/firebase/Firebase.js
+++ src/components/firebase/Firebase.js
@@ -2,14 +2,14 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBlQVjQoto6uNR50GGQ8Wae0q8ZidQXExs",
-  authDomain: "course-schedule-recommender.firebaseapp.com",
-  databaseURL: "https://course-schedule-recommender.firebaseio.com",
-  projectId: "course-schedule-recommender",
-  storageBucket: "course-schedule-recommender.appspot.com",
-  messagingSenderId: "887967754562",
-  appId: "1:887967754562:web:30abe3545abe567fd740db",
-  measurementId: "G-YNBQT4EPQ1"
+  apiKey: ###INSERT KEY###,
+  authDomain: ###INSERT KEY###,
+  databaseURL: ###INSERT KEY###,
+  projectId: ###INSERT KEY###,
+  storageBucket: ###INSERT KEY###,
+  messagingSenderId: ###INSERT KEY###,
+  appId: ###INSERT KEY###,
+  measurementId: ###INSERT KEY###
 };
 
 class Firebase {
